e80559a5890e124e5ca96c821068e7e4d4c33ba9
RockLikeBusStop <arasam1067@gmail.com>
2020-05-23 02:21:31 -0400

    If-Else Statements I played along with if, else if, and else statements.

--- main.rs
+++ main.rs
@@ -1,7 +1,12 @@
 fn main() {
     let mut x = 45;
-    let mut y = 45;
     println!("x is {}", x);
-    x = 20;
+    x = 90;
     println!("x is changed to {}", x);
+
+    if x < 30 {
+        println!("x is less than 30");
+    } else {
+        println!("x is greater than or equal to 30");
+    }
 }
